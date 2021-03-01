a940e481710cf844813ee3334995ac9e8bd0cb95
YakunichkinAV <yakunichkinav@multibonus.ru>
2021-03-02 02:14:24 +0300

    add test

---
+++ src/test/java/tests/MyTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyTest {
+
+    @Test
+    void myTest(){
+        assertTrue(true);
+    }
+}
